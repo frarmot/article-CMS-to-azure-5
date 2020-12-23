90cccd04c1110015671cc11e9c20fa044cfe00ce
Fred Motari <faruyam@gmail.com>
2020-12-23 04:10:44 -0600

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,9 +11,10 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
-streamHandler = logging.getLogger('azure')
-streamHandler.setLevel(logging.INFO)
-app.logger.addHandler(streamHandler)
+logger = logging.getLogger('azure')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+logger.addHandler(handler)
 app.logger.setLevel(logging.INFO)
 #
 Session(app)
